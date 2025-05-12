59e290a0c3349bd71988ba3adc9a86df3c1308e1
MisterB <babbag3@hotmail.com>
2025-05-12 14:44:53 +0100

    Updated icons + mappings

--- include/IconMappings.h
+++ include/IconMappings.h
@@ -1,10 +1,14 @@
 #pragma once
 
-// Icons
+// Reminders:
+// Top left start letter is 0x20
+
+// Logo
 #define Icon_Logo_Start1 0xB0
 #define Icon_Logo_Start2 0xC0
 #define Icon_Logo_Start3 0xD0
 
+// Battery
 #define Icon_Battery 0x6D
 #define Icon_BatteryCharging 0x6E
 #define Icon_BatteryEmpty 0x6F
@@ -12,22 +16,19 @@
 #define Icon_BatteryBigEmpty2 0x7E
 #define Icon_BatteryBigEmpty3 0x7F
 
+// LED
 #define Icon_LEDOn 0X87
 #define Icon_LEDOff 0X88
 #define Icon_LEDNone 0X89
 
+// Bluetooth
 #define Icon_EyesLeft 0x79
 #define Icon_EyesRight 0x7A
 #define Icon_BTOK 0x7B
 #define Icon_BTLogo 0x7C
 //#define Icon_Heart 0x7C
 
-#define Icon_Start 0x8A
-#define Icon_Select1 0x8B
-#define Icon_Select2 0x8C
-
-#define Icon_Tilt 0x57
-
+// USB
 #define Icon_Wire_Horizontal 0x66
 #define Icon_USB_Unknown 0x67
 #define Icon_USB_Connected 0x68
@@ -36,10 +37,37 @@
 #define Icon_USB_Connected_V 0x6B
 #define Icon_USB_Disconnected_V 0x6C
 
+// WiFi
+#define Icon_WIFI_TraceSignal 0x97
+#define Icon_WIFI_LowSignal 0x98
+#define Icon_WIFI_MediumSignal 0x99
+#define Icon_WIFI_MiddleSignal 0x9A
+#define Icon_WIFI_TopSignal 0x9B
+#define Icon_WIFI_HighSignal 0x9C
+#define Icon_WIFI_Query 0x9D
+#define Icon_WIFI_LostSignal 0x9E
+#define Icon_WIFI_Disabled 0x9F
+
+// WebServer
+#define Icon_Web_Disabled 0xA8
+#define Icon_Web_Enabled 0xB8
+#define Icon_Web_Traffic 0xC8
+
+// Misc.
+#define Icon_Settings 0xA7
+
+// Extra buttons
+#define Icon_Start 0x8A
+#define Icon_Select1 0x8B
+#define Icon_Select2 0x8C
+
+#define Icon_Tilt 0x57
+
 #define Icon_VolUp 0x8D
 #define Icon_VolDown 0x8E
 #define Icon_VolMute 0x8F
 
+// DPad
 #define Icon_DPad_Icon 0x26
 #define Icon_DPad_Neutral 0x27
 #define Icon_DPad_Up 0x28
@@ -51,6 +79,7 @@
 #define Icon_DPad_Left 0x2E
 #define Icon_DPad_UpLeft 0x2F
 
+// Guitar 1 Gfx
 #define Icon_Guitar1_T1 0x20
 #define Icon_Guitar1_T2 0x21
 #define Icon_Guitar1_T3 0x22
@@ -68,6 +97,7 @@
 #define Icon_Guitar1_CenterBottom 0x42
 #define Icon_Guitar1_CenterOn 0x43
 
+// Guitar 2 Gfx
 #define Icon_Guitar2_T1 0x50
 #define Icon_Guitar2_T2 0x51
 #define Icon_Guitar2_T3 0x52
@@ -85,6 +115,7 @@
 #define Icon_Guitar2_CenterBottom 0x72
 #define Icon_Guitar2_CenterOn 0x73
 
+// Joypad low states
 #define Icon_JPad_Icon 0x36
 #define Icon_JPad_Neutral 0x37
 #define Icon_JPad_Low_Up 0x38
@@ -96,6 +127,7 @@
 #define Icon_JPad_Low_Left 0x3E
 #define Icon_JPad_Low_UpLeft 0x3F
 
+// Joypad medium states
 #define Icon_JPad_Med_Up 0x48
 #define Icon_JPad_Med_UpRight 0x49
 #define Icon_JPad_Med_Right 0x4A
@@ -105,6 +137,7 @@
 #define Icon_JPad_Med_Left 0x4E
 #define Icon_JPad_Med_UpLeft 0x4F
 
+// Joypad high states
 #define Icon_JPad_Hi_Up 0x58
 #define Icon_JPad_Hi_UpRight 0x59
 #define Icon_JPad_Hi_Right 0x5A
@@ -114,6 +147,7 @@
 #define Icon_JPad_Hi_Left 0x5E
 #define Icon_JPad_Hi_UpLeft 0x5F
 
+// Empty circles
 #define Icon_EmptyCircle_1 0xA9
 #define Icon_EmptyCircle_2 0xAA
 #define Icon_EmptyCircle_3 0xAB
@@ -129,6 +163,7 @@
 #define Icon_EmptyCircle_13 0xBE
 #define Icon_EmptyCircle_14 0xBF
 
+// Filled circles
 #define Icon_FilledCircle_1 0xC9
 #define Icon_FilledCircle_2 0xCA
 #define Icon_FilledCircle_3 0xCB
